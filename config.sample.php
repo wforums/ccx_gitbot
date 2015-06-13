5a9bbcd05eec7513517d4d34ec084d8b87f6b54f
wforums <webmaster@wforums.net>
2015-06-13 03:13:41 +0200

    Further experimenting
    Switched github client, added more code. Still a lot to do.

--- config.sample.php
+++ config.sample.php
@@ -2,4 +2,11 @@
 /**
  * Rename this file to config.php and fill in the values.
  */
+// The OAUTH token that you can generate on https://github.com/settings/tokens. Needs access to: // TODO: fill this in
 define("CCX_OAUTH_TOKEN","GitHub token here");
+// The repository where we're working in. Will ignore other repositories (for security reasons).
+define("CCX_REPO_NAME","ccx_gitbot");
+// The name of the organisation or person that owns the repository.
+define("CCX_REPO_OWNER","wforums");
+// The username of the GitHub bot
+define("BOT_USERNAME","ccx-testerbot");
