1f032560911a440623a78473340c193cef3ab886
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-06-13 03:13:41 +0200

    Further experimenting
    Switched github client, added more code. Still a lot to do.

--- bot.php
+++ bot.php
@@ -1,8 +1,60 @@
 <?php
+use Github\Api\Notification;
+use Github\Client;
+
 include "vendor/autoload.php";
+require "config.php";
 
-$client = new GitHubClient();
-$client->setDebug(true);
-$client->setOauthKey(CCX_OAUTH_TOKEN);
+/** @var  $client */
+$client = new Client();
 
-var_dump($client->activity);
+// Authenticate with token instead of user & pass, this is safer.
+$client->authenticate(CCX_OAUTH_TOKEN,null,Client::AUTH_HTTP_TOKEN);
+
+// Get the newest notifications
+$notifications = $client->notifications()->all();
+
+foreach($notifications as $notification){
+    // Verify repository
+    if($notification["repository"]["full_name"] === CCX_REPO_OWNER.'/'.CCX_REPO_NAME){
+        // Get problem/PR
+        $urlParts = explode("/",$notification["subject"]["url"]);
+        $problemNr= intval($urlParts[sizeof($urlParts)-1]);
+        // Get type
+        $type = $notification["subject"]["type"];
+
+        $first_comment = null;
+        $comments = [];
+
+        switch($type){
+            case "Issue":
+                // Get issue
+                $comments = $client->issues()->comments()->all(CCX_REPO_OWNER,CCX_REPO_NAME,$problemNr);
+                $first_comment = $client->issues()->show(CCX_REPO_OWNER,CCX_REPO_NAME,$problemNr);
+                break;
+            case "":
+                // TODO: do same for pull request
+                break;
+            default:
+                break;
+        }
+
+        if($first_comment === null){
+            continue;
+        }
+
+        // Go from end to comments up to the first, check for mentions
+        foreach($comments as $comment){
+            if(strpos($comment["body"],"@".BOT_USERNAME) === false){
+                continue;
+            }
+            // Mention found
+            var_dump($comment);
+        }
+
+        //var_dump($first_comment);
+        //var_dump($comments);
+    }
+}
+
+$client->notifications()->markRead(new DateTime());
