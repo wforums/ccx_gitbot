306b4df84d76045e9d3dc8be886804e8a7213707
wforums <webmaster@wforums.net>
2015-06-26 00:14:04 +0200

    Updates to the bot
    - Nearly ready for use
    - 3 other parts to finish:
    * Code for launching VB
    * Code inside VB
    * PHP that the VB reports to

---
+++ botMessages.py
@@ -0,0 +1,8 @@
+__author__ = 'Willem'
+
+acknowledged = 'Command acknowledged'
+invalidCommand = 'No valid command detected. Please try again.'
+branchMissing = 'No branch was specified. Please use "runtests {branch ' \
+                'name}".'
+branchInvalid = 'Given branch {0} is invalid. Please select a valid branch ' \
+                'name.'
