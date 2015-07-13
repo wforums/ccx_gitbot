43f52bc8c858d2df00b3b664b9c8368a233447ce
wforums <webmaster@wforums.net>
2015-07-13 05:39:33 +0200

    Start of worker
    Beginning of worker

---
+++ worker/private/variables-sample.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Willem
+ *
+ * Copy and rename this file to variables.php and fill in the necessary values in order for the script to work
+ */
+// User agent, must be equal to the user agent in the command server settings
+define("CCX_USER_AGENT_C","My username here");
