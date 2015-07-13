4df6e08c00279ec5babb8e98c8f63b4604882626
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-07-13 05:38:11 +0200

    GitHub bot fixes
    Fixed some issues with the scripts

--- bot/run_vm.py
+++ bot/run_vm.py
@@ -4,9 +4,8 @@ import pymysql
 import virtualbox
 import virtualbox.library as library
 from virtualbox.library_base import VBoxError
-
-from bot import configuration
-from bot.configuration import Configuration
+from configuration import Configuration
+from subprocess import call
 
 '''
     A script to run the test suite inside a VirtualBox VM. Be warned,
@@ -40,60 +39,78 @@ def main():
 
     :return:
     """
-    conn = pymysql.connect(host=configuration.database_host,
-                           user=configuration.database_user,
-                           passwd=configuration.database_password,
-                           db=configuration.database_name,
+    conn = pymysql.connect(host=Configuration.database_host,
+                           user=Configuration.database_user,
+                           passwd=Configuration.database_password,
+                           db=Configuration.database_name,
                            charset='latin1',
                            cursorclass=pymysql.cursors.DictCursor)
+
+    # TODO: add debug info
 
     # Obtain the  last queue item (if any)
     result = None
     try:
         with conn.cursor() as c:
-            c.execute("SELECT test_id FROM queue ORDER BY ROWID ASC LIMIT 1")
+            c.execute(
+                "SELECT test_id FROM test_queue ORDER BY test_id LIMIT 1")
             result = c.fetchone()
+
+            if result is None:
+                print("No items in queue left, aborting...")
+                return
+
+            test_id = result['test_id']
+            print("Processing id {0}".format(test_id))
+            c.execute("SELECT token, repository, branch, commit_hash, type "
+                      "FROM test WHERE id = %s", (test_id,))
+            result = c.fetchone()
+            print("Running tests for {0} (branch {1}, commit {2})"
+                  " with token {3}".format(result['repository'],
+                                           result['branch'],
+                                           result['commit_hash'],
+                                           result['token']))
     finally:
         conn.close()
-
-    if result is None:
-        print("No items in queue left, aborting...")
-        return
-
-    test_id = result[0]
-    print("Processing id {0}".format(test_id))
-    c.execute("SELECT token, repository, branch, commit_hash, type FROM tests "
-              "WHERE id = ?", (test_id,))
-    result = c.fetchone()
-    print("Running tests for {repo} (branch {branch}, commit {commit}) with "
-          "token {token}".format(repo=result[1], branch=result[2],
-                                 commit=result[3], token=result[0]))
-    c.close()
-    conn.close()
 
     # Setting up VBox
     virtual_box = virtualbox.VirtualBox()
     session = virtualbox.Session()
 
-    vm = virtual_box.find_machine('CCX_VM')
+    vm = virtual_box.find_machine(Configuration.vbox_name)
 
     # TODO: if VM is still running, and the maximum time is elapsed,
     # force shutdown.
 
     session2 = virtualbox.Session()
-    vm.lock_machine(session2, library.LockType.write)
+    vm.lock_machine(session2, library.LockType.shared)
+    print("Obtained lock: {0}".format(session2.type_p))
 
-    if session2.machine.current_state_modified:
-        print("Restoring snapshot")
-        progress = session2.console.restore_snapshot(vm.current_snapshot)
-        progress.wait_for_completion()
-        print("Snapshot should be restored")
-
-    session2.unlock_machine()
+    try:
+        if session2.machine.current_state_modified:
+            print("Restoring snapshot")
+            if Configuration.use_vbox_manage:
+                ret = call([
+                    "VBoxManage",
+                    "snapshot",
+                    Configuration.vbox_name,
+                    "restorecurrent"
+                ])
+                print("VBoxManage process exited with  {0}".format(ret))
+            else:
+                console = session2.console
+                progress = console.restore_snapshot(vm.current_snapshot)
+                progress.wait_for_completion(-1)
+            print("Snapshot should be restored")
+    except Exception as e:
+        print("Could not retrieve state info; skipping")
+        traceback.print_exc()
+    finally:
+        session2.unlock_machine()
 
     try:
         progress = vm.launch_vm_process(session, 'vrdp', '')
-        progress.wait_for_completion()
+        progress.wait_for_completion(-1)
     except VBoxError:
         print("Machine is already running!")
         exit()
@@ -101,44 +118,80 @@ def main():
     print("Trying to create a session")
     t = 0
     gs = None
-    while t != 1:
+    gs_status = 0
+    while t != library.GuestSessionWaitResult.start and gs_status != \
+            library.GuestSessionStatus.started:
         try:
-            gs = session.console.guest.create_session(
-                Configuration.vbox_user, Configuration.vbox_password)
-            t = gs.wait_for(1, 120000)
-            print("Session wait result was {0}".format(t))
+            if gs is None:
+                gs = session.console.guest.create_session(
+                    Configuration.vbox_user, Configuration.vbox_password)
+            t = gs.wait_for(1, 0)
+            gs_status = gs.status
+            print("Session wait result was {0}, session status is {1}"
+                  "".format(t, gs_status))
             time.sleep(5)
         except SystemError:
             print("Failed to start... Keep trying")
+            gs = None
     print("Session created")
+    print("Waiting 120 seconds for machine to boot")
+    time.sleep(120)
 
-    try:
-        process = gs.process_create(
-            '/bin/bash',
-            [
-                Configuration.vbox_script,
-                result[0],  # token
-                result[1],  # GitHub git location
-                result[2],  # branch
-                result[3],  # commit
-                ">",
-                Configuration.result_folder + "/log.html",
-                "2>",
-                "&1"
-            ],
-            [],
-            [library.ProcessCreateFlag.wait_for_process_start_only],
-            0)
-        t = process.wait_for(library.ProcessWaitForFlag.start)
-        print("Process wait result was {0}".format(t))
-    except Exception as e:
-        print("Error occurred: {0}".format(e))
-        traceback.print_exc()
+    loop = True
+    while loop:
+        try:
+            flags = [library.ProcessCreateFlag.wait_for_process_start_only]
+            if Configuration.debug:
+                flags = [
+                    library.ProcessCreateFlag.wait_for_std_out,
+                    library.ProcessCreateFlag.wait_for_std_err
+                ]
+            process = gs.process_create(
+                Configuration.vbox_script,  #'/bin/bash',
+                [
+                    result['token'],  # token
+                    result['repository'],  # GitHub git location
+                    result['branch'],  # branch
+                    result['commit_hash']   # commit
+                ],
+                [],
+                flags,
+                0)
+            t = process.wait_for(library.ProcessWaitForFlag.start)
+            print("Process wait result was {0}".format(t))
+            if Configuration.debug:
+                while process.status == library.ProcessStatus.started:
+                    data = process.read(1, 4096, 0)
+                    data2 = process.read(2, 4096, 0)
+                    if len(data) > 0:
+                        print("stdout: {0}".format(len(data)))
+                        try:
+                            print(data[0:-1].tobytes().decode('utf-8'))
+                        except AttributeError:
+                            print(data[0:-1])
+                    if len(data2) > 0:
+                        print("stderr: {0}".format(len(data2)))
+                        try:
+                            print(data2[0:-1].tobytes().decode('utf-8'))
+                        except AttributeError:
+                            print(data2[0:-1])
+                    time.sleep(1)
+
+            loop = False
+        except library.VBoxErrorIprtError as e:
+            print("Seems the system is not ready yet... Will try again in "
+                  "5 seconds")
+            print("{0:#08x} ({1})".format(e.value,e.msg))
+            time.sleep(5)
+        except Exception as e:
+            print("Error occurred: {0}".format(e))
+            traceback.print_exc()
+            loop = False
 
     # No need to shut down the machine, the script should be able to do
     # this by itself.
     print("Disconnecting session")
     session.unlock_machine()
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
