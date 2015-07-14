3a95927fa1b051dcea93f679fc58b0cf07747791
wforums <webmaster@wforums.net>
2015-07-14 02:56:53 +0200

    Worker work
    Continued work on worker node, ready for testing

--- worker/private/variables-sample.php
+++ worker/private/variables-sample.php
@@ -7,3 +7,9 @@
  */
 // User agent, must be equal to the user agent in the command server settings
 define("CCX_USER_AGENT_C","My username here");
+// User agent for reply back, must be equal to the user agent in the command server settings
+define("CCX_USER_AGENT_S","My user agent here");
+// Location to fetch the data from
+define("MASTER_SERVER_URL","http://my.master.server/fetch.php");
+// Location of the test script
+define("TEST_SCRIPT_LOCATION","/path/to/script/private/runLocal");
