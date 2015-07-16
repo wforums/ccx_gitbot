e1270bc018a02c0bc8d72c9e805337a8f65518ad
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-07-16 08:21:26 +0200

    Worker fixes
    Fixes in both the worker bash script and in the php

--- worker/public/command.php
+++ worker/public/command.php
@@ -3,7 +3,6 @@
  * Created by PhpStorm.
  * User: Willem
  */
-
 include_once "../private/variables.php";
 
 $command = "INVALID COMMAND";
@@ -14,23 +13,25 @@ if ($_SERVER['HTTP_USER_AGENT'] === CCX_USER_AGENT_C) {
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, MASTER_SERVER_URL);
-        curl_setopt($ch, CURLOPT_USERAGENT, CCX_USER_AGENT_C);
+        curl_setopt($ch, CURLOPT_USERAGENT, CCX_USER_AGENT_S);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Need to return the result to the variable
         curl_setopt($ch, CURLOPT_POST, true);
         // Add token as post variable
-        curl_setopt($ch, CURLOPT_POSTFIELDS, ['token' => $_POST['token']]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array ('token' => $_POST['token']));
 
         $data = curl_exec($ch);
         curl_close($ch);
-        if ($data !== false) {
-            $parsed = json_decode($data);
+        if ($data !== false && $data !== 'INVALID COMMAND') {
+            $parsed = json_decode($data,true);
             if ($parsed !== null) {
                 // Got valid JSON
                 if($parsed["status"] === "success"){
                     // Init shell script with variables
-                    exec(TEST_SCRIPT_LOCATION." ".escapeshellarg($parsed["token"]." ".escapeshellarg($parsed["git"]).
-                            " ".escapeshellarg($parsed["branch"])." ".escapeshellarg($parsed["commit"])." &"));
+                    $cmd = TEST_SCRIPT_LOCATION." ".escapeshellarg($parsed["token"])." ".escapeshellarg($parsed["git"]).
+                            " ".escapeshellarg($parsed["branch"])." ".escapeshellarg($parsed["commit"])."> /home/willem/worker/log.txt 2>&1 &";
+                    exec($cmd,$array,$status);
                 }
+                $command = "OK";
             }
         }
     }
