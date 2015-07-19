72450d9b32aca49f49b88b1ea415e04f95ec81d3
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-07-19 08:07:06 +0200

    View update
    The view is now a bit nicer.

--- server/public/view.php
+++ server/public/view.php
@@ -2,6 +2,8 @@
 
 include_once "../private/variables.php";
 
+echo '<html><head><link href="layout.css" rel="stylesheet" /></head><body>';
+echo "<h1>Progress for test request</h1>";
 if(isset($_GET["id"])){
     $pdo = new PDO(DATABASE_SOURCE_NAME,DATABASE_USERNAME,DATABASE_PASSWORD, [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
@@ -15,29 +17,27 @@ if(isset($_GET["id"])){
         $testEntry = $stmt->fetch();
         if($testEntry === false){
             // No id like this exists
-            echo "Invalid id";
+            echo "<p>Invalid id</p>";
         } else {
             $stmt = $pdo->prepare("SELECT * FROM test_progress WHERE test_id = :id ORDER BY id ASC;");
             $stmt->bindParam(":id",$testEntry["id"],PDO::PARAM_INT);
             if($stmt->execute()){
                 $data = $stmt->fetchAll();
+                $url = str_replace(".git","",str_replace('git://','https://',$testEntry["repository"]));
+                switch($testEntry["type"]){
+                    case "Commit":
+                        $url .= "/commit/".$testEntry["commit_hash"];
+                        echo "<p>Testing repository ".htmlentities($testEntry["repository"])." in branch ".htmlentities($testEntry["branch"]).' for commit <a href="'.htmlentities($url).'">'.htmlentities($testEntry["commit_hash"])."</a></p>";
+                        break;
+                    case "PullRequest":
+                        $url .= "/pull/".$testEntry["commit_hash"];
+                        echo "<p>Testing repository ".htmlentities($testEntry["repository"]).' for <a href="'.htmlentities($url).'">pull request '.htmlentities($testEntry["commit_hash"])."</a></p>";
+                        break;
+                    default:
+                        echo "<p>Unknown test type!</p>";
+                        break;
+                }
                 if(sizeof($data) >= 1){
-                    echo '<html><head><link href="layout.css" rel="stylesheet" /></head><body>';
-                    echo "<h1>Progress for test request</h1>";
-                    $url = str_replace(".git","",str_replace('git://','https://',$testEntry["repository"]));
-                    switch($testEntry["type"]){
-                        case "Commit":
-                            $url .= "/commit/".$testEntry["commit_hash"];
-                            echo "<p>Testing repository ".htmlentities($testEntry["repository"])." in branch ".htmlentities($testEntry["branch"]).' for commit <a href="'.htmlentities($url).'">'.htmlentities($testEntry["commit_hash"])."</a></p>";
-                            break;
-                        case "PullRequest":
-                            $url .= "/pull/".$testEntry["commit_hash"];
-                            echo "<p>Testing repository ".htmlentities($testEntry["repository"]).' for <a href="'.htmlentities($url).'">pull request '.htmlentities($testEntry["commit_hash"])."</a></p>";
-                            break;
-                        default:
-                            echo "Unknown test type!";
-                            break;
-                    }
                     echo "<table>";
                     echo "<tr><th>Time</th><th>Status</th><th>Message</th></tr>";
                     foreach($data as $row){
@@ -49,15 +49,15 @@ if(isset($_GET["id"])){
                         echo '<p><strong>WARNING! The result files have been auto-generated and could possibly contain malware*, so please use caution.</strong></p>';
                         echo '<p>(* That is, if someone is so unkind to abuse the functionality of the bot)</p>';
                     }
-                    echo '</body></html>';
                 } else {
-                    echo "Still in the waiting queue. Please return later";
+                    echo '<p>Still in the waiting queue. Please return later</p>';
                 }
             }
         }
     } else {
-        echo "Invalid id";
+        echo "<p>Invalid id</p>";
     }
 } else {
-    echo "No id specified";
+    echo "<p>No id specified</p>";
 }
+echo '</body></html>';
