04f394ed39c91bc482ebc55daab64321c446f347
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-08-24 11:48:30 +0200

    URL changes
    -

--- server/public/index.php
+++ server/public/index.php
@@ -13,7 +13,7 @@
     <body>
         <h1>CCExtractor GSoC* 2015 project: GitHub bot</h1>
         <p>Welcome to the server that runs this project! If you want to have more information about this project, you
-            can visit the GitHub page for this project: <a href="https://github.com/wforums/ccx_gitbot">CCX Git bot</a>.
+            can visit the GitHub page for this project: <a href="https://github.com/canihavesomecoffee/ccx_gitbot">CCX Git bot</a>.
         </p>
         <p><small>*: Google Summer of Code</small></p>
     </body>
